e3b455650ed8cb7918723673338a235b8f7022ec
wivulo <walterivulo@gmail.com>
2022-04-08 00:39:57 +0100

    terceiro commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import { BrowserRouter ,Route, Routes, Outlet } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from "./auth/login/login";
 import Register from "./auth/register/Register";
 
-function Header(){
-	return (
-            <header className="a-header a-header-1">
-		<div className="a-block a-block-1 flex flex-row flex-h-center">
-			<div className="col-1">
-				<a href="index.html" className="a-txt txt-1 decomart fs-medium f-600">aa</a>
-    			</div>
-        
-                    <div className="col-2">
-                    	<h5 className="a-txt txt-2 roboto-regular" id="title">Acrônimos</h5>
-                    </div>
-                    
-                    <div className="col-3"></div>
-                </div>
-            </header>
-            
-	);
+function Header() {
+  return (
+    <header className="a-header a-header-1">
+      <div className="a-block a-block-1 flex flex-row flex-h-center">
+        <div className="col-1">
+          <a href="index.html" className="a-txt txt-1 decomart fs-medium f-600">aa</a>
+        </div>
+
+        <div className="col-2">
+          <h5 className="a-txt txt-2 roboto-regular" id="title">Acrônimos</h5>
+        </div>
+
+        <div className="col-3"></div>
+      </div>
+    </header>
+
+  );
 }
 
-function Dashboard({ children }){
-	return (
-		 <section className="a-section dashboard" id="dashboard">
-			{ children }
-		</section>
-	);
+function Dashboard({ children }) {
+  return (
+    <section className="a-section dashboard" id="dashboard">
+      {children}
+    </section>
+  );
 }
 
 function App() {
   return (
     <BrowserRouter>
-  <div className='App bg-darkblue-palette txt-color-smoke'>
-    <Header />
-    <Dashboard>
-      <Routes>
-        <Route element = { <Login /> }  path="/" exact />
-        <Route element = { <Login /> }  path="/login" />
-        <Route element = { <Register /> }  path="/register" />
-      </Routes>
-    </Dashboard>
-  </div>
-  </BrowserRouter>
+      <div className='App bg-darkblue-palette txt-color-smoke'>
+        <Header />
+        <Dashboard>
+          <Routes>
+            <Route element={<Login />} path="/" exact />
+            <Route element={<Login />} path="/login" />
+            <Route element={<Register />} path="/register" />
+          </Routes>
+        </Dashboard>
+      </div>
+    </BrowserRouter>
   );
 }
 
